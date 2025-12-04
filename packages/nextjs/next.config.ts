9d9f209ac2dc028841abe9ea42679c549abecb90
lizgeorge1 <lizmerin.george@gmail.com>
2025-12-05 03:28:55 +0530

    Config updated

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@solana-program/system": false,
+      "@solana-program/memo": false,
+      "@solana-program/token": false,
+      "@solana/kit": false,
+    };
+
     return config;
   },
 };
