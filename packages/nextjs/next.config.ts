d400cf23fee45f0cb7b82ea38274ccaad4af7065
George Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2025-12-09 22:29:06 +0530

    standalone

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -2,6 +2,7 @@ import withSerwist from "@serwist/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   devIndicators: false,
   typescript: {
